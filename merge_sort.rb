44a355bdcadf8cb011cdd4adb1b3b7b824106bb4
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-06-16 20:01:39 -0400

    \rubocop pass

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,34 +1,31 @@
+# frozen_string_literal: true
+
 # define merge sort with merge
 
 def merge_sort(array)
-    #base case
-    return array if array.length < 2
-    #split array
-    middle = (array.length / 2)
-    left = array[0...middle]
-    right = array[(middle)...(array.length)]
-    
-    sorted_left = merge_sort(left)
-    sorted_right = merge_sort(right)
-    merge(sorted_left,sorted_right) 
-   
+  # base case
+  return array if array.length < 2
+
+  # split array
+  middle = (array.length / 2)
+  left = array[0...middle]
+  right = array[middle...(array.length)]
+
+  sorted_left = merge_sort(left)
+  sorted_right = merge_sort(right)
+  merge(sorted_left, sorted_right)
 end
 
-def merge(left,right,sorted=[])
-    until left.empty? || right.empty?
-        if left.first < right.first
-            sorted << left.shift
-        else
-            sorted << right.shift
-        end
-    end
-    sorted + left + right # catchall once one of the array is empty. the other should be sorted.
+def merge(left, right, sorted = [])
+  until left.empty? || right.empty?
+    sorted << if left.first < right.first
+                left.shift
+              else
+                right.shift
+              end
+  end
+  sorted + left + right # catchall once one of the array is empty. the other should be sorted.
 end
 
-
-
-
-test = [3,4,1,2,6,5]
+test = [3, 4, 1, 2, 6, 5]
 p merge_sort(test)
-
-
--- recursion.rb
+++ recursion.rb
@@ -1,35 +1,35 @@
+# frozen_string_literal: true
+
 # fibonacci using iteration
 
 def fibs(n)
-    array = []
-    i = 0
-    while i <= n
-        if i == 0
-            array << 0
-        elsif i == 1
-            array << 1
-        else
-            num =  array[i-1] + array [i-2]
-            array << num
-        end
-        i += 1
+  array = []
+  i = 0
+  while i <= n
+    if i == 0
+      array << 0
+    elsif i == 1
+      array << 1
+    else
+      num = array[i - 1] + array [i - 2]
+      array << num
     end
-    array
+    i += 1
+  end
+  array
 end
 
 def fibs_rec(n)
-    
-    if n == 0
-        return [0]
-        
-    elsif n == 1
-        return [0,1]
-    end
-    array = fibs_rec(n-1)
-    array << array[-2] + array[-1]
+  if n == 0
+    return [0]
+
+  elsif n == 1
+    return [0, 1]
+  end
+
+  array = fibs_rec(n - 1)
+  array << array[-2] + array[-1]
 end
-
-
 
 p fibs(10)
 p fibs_rec(10)
